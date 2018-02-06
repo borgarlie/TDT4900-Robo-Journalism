Fixed GAN policy loss calculation

models/seq2seq/decoder.py
@@ -25,7 +25,7 @@ class AttnDecoderRNN(nn.Module):
 
         cat = torch.cat((embedded[0], hidden[0]), 1)
         temp = self.attn(cat)
-        attn_weights = F.softmax(temp)
+        attn_weights = F.softmax(temp, dim=1)
         attn_applied = torch.bmm(attn_weights.unsqueeze(1), encoder_outputs.transpose(0, 1))
 
         output = torch.cat((embedded[0], attn_applied.squeeze(1)), 1)
@@ -33,5 +33,6 @@ class AttnDecoderRNN(nn.Module):
 
         output, hidden = self.gru(output, hidden)
 
-        output = F.log_softmax(self.out(output[0]))
+        # TODO: Should dim = 0 ?
+        output = F.log_softmax(self.out(output[0]), dim=1)
         return output, hidden, attn_weights

training/GAN/run_experiment.py
@@ -163,6 +163,7 @@ if __name__ == '__main__':
     generator_encoder_optimizer = optim.SGD(generator_encoder.parameters(), lr=generator_learning_rate)
     generator_decoder_optimizer = optim.SGD(generator_decoder.parameters(), lr=generator_learning_rate)
     generator_mle_criterion = torch.nn.NLLLoss()
+    policy_criterion = torch.nn.NLLLoss(reduce=False)
 
     # TODO: should this one be loaded?
     discriminator_optimizer = torch.optim.Adam(discriminator_model.parameters(), lr=discriminator_learning_rate)
@@ -172,8 +173,8 @@ if __name__ == '__main__':
     generator_beta.update_params(generator_encoder, generator_decoder)
 
     generator = Generator(generator_encoder, generator_decoder, generator_encoder_optimizer,
-                          generator_decoder_optimizer, generator_mle_criterion, batch_size, use_cuda, beta,
-                          generator_beta)
+                          generator_decoder_optimizer, generator_mle_criterion, policy_criterion, batch_size, use_cuda,
+                          beta, generator_beta)
 
     discriminator = Discriminator(discriminator_model, discriminator_optimizer, discriminator_criterion)
 