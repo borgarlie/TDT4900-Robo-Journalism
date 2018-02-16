cf1c6d945bf9801d7788213ca8acb74b1ab89731
Borgar Lie <borgar.lie@gmail.com>
2018-02-16 12:01:49 +0100

    Fix fake data generation with pickle files and pointer generator

--- evaluation/seq2seq/generate_fake_data.py
+++ evaluation/seq2seq/generate_fake_data.py
@@ -3,52 +3,66 @@ import os
 
 sys.path.append('../..')  # ugly dirtyfix for imports to work
 
-from models.seq2seq.decoder import AttnDecoderRNN
+from models.seq2seq.decoder import PointerGeneratorDecoder
 from models.seq2seq.encoder import EncoderRNN
-from preprocess import preprocess
+from preprocess.preprocess_pointer import *
 from utils.data_prep import *
 
 
-def generate_argmax_summaries(vocabulary, encoder, decoder, articles, max_length, with_categories=False):
+minimum_abstract_length = 4
+
+
+def generate_argmax_summaries(vocabulary, encoder, decoder, summary_pairs, max_article_length, max_abstract_length):
     fake_data = []
     print_every = 500
-    for i in range(0, len(articles)):
-        if with_categories:
-            _, article = split_category_and_article(articles[i])
-        else:
-            article = articles[i]
-        input_variable = indexes_from_sentence(vocabulary, article)
-        input_variable = pad_seq(input_variable, max_length)
-        input_length = max_length
+    for i in range(0, len(summary_pairs)):
+        article = summary_pairs[i].unked_article_tokens
+        # TODO: Is it necessary to PAD here?
+        input_variable = pad_seq(article, max_article_length)
+        input_length = max_article_length
         input_variable = Variable(torch.LongTensor(input_variable)).unsqueeze(1)
         input_variable = input_variable.cuda() if use_cuda else input_variable
-        fake_sample = generate_single_argmax_summary(vocabulary, encoder, decoder, input_variable, input_length)
+
+        full_article = summary_pairs[i].article_tokens
+        full_input_variable = pad_seq(full_article, max_article_length)
+        full_input_variable = Variable(torch.LongTensor(full_input_variable)).unsqueeze(0)
+        full_input_variable = full_input_variable.cuda() if use_cuda else full_input_variable
+
+        fake_sample = generate_single_argmax_summary(vocabulary, encoder, decoder, input_variable, full_input_variable,
+                                                     input_length, max_abstract_length)
         fake_data.append(' '.join(fake_sample))
         if (i+1) % print_every == 0:
-            print("Done creating : %d / %d samples" % ((i+1), len(articles)), flush=True)
+            print("Done creating : %d / %d samples" % ((i+1), len(summary_pairs)), flush=True)
     print("Done creating fake data", flush=True)
     return fake_data
 
 
-def generate_single_argmax_summary(vocabulary, encoder, decoder, input_variable, input_length):
+def generate_single_argmax_summary(vocabulary, encoder, decoder, input_variable, full_input_variable, input_length,
+                                   max_abstract_length):
     encoder_outputs, encoder_hidden = encoder(input_variable, [input_length], None)
     encoder_hidden = concat_encoder_hidden_directions(encoder_hidden)
     decoder_input = Variable(torch.LongTensor([SOS_token]))
     decoder_input = decoder_input.cuda() if use_cuda else decoder_input
     decoder_hidden = encoder_hidden
     decoded_word_sequence = []
-    for di in range(input_length):
-        decoder_output, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_hidden, encoder_outputs, 1)
+    for di in range(max_abstract_length):
+        decoder_output, decoder_hidden, decoder_attention \
+            = decoder(decoder_input, decoder_hidden, encoder_outputs, full_input_variable, 1)
         topv, topi = decoder_output.data.topk(3)
         next_word = topi[0][0]
-        # need special logic to ensure minimum 4 words
+        # need special logic to ensure minimum words
         if next_word == EOS_token or next_word == PAD_token:
-            if di >= 4:
+            if di >= minimum_abstract_length:
                 break
             counter = 1
             while next_word == EOS_token or next_word == PAD_token:
                 next_word = topi[0][counter]
                 counter += 1
+
+        # replace OOV words with UNK
+        if next_word >= vocabulary.n_words:
+            next_word = UNK_token
+
         decoded_word = vocabulary.index2word[next_word]
         decoded_word_sequence.append(decoded_word)
         # next input to decoder
@@ -75,21 +89,22 @@ if __name__ == '__main__':
         torch.cuda.set_device(int(sys.argv[1]))
         print("Using GPU: %s" % sys.argv[1], flush=True)
 
-    relative_path = "../../data/ntb_preprocessed/ntb_80_5cat.unk"
+    relative_path = "../../data/ntb_pickled/ntb_pointer_30k"
     hidden_size = 128
+    embedding_size = 100
     n_layers = 1
     dropout_p = 0.1
-    load_file = "../../models/pretrained_models/pretrained_seq2seq.pth.tar"
-    with_categories = False
+    load_file = "../../models/pretrained_models/ntb/pretrained_pointer.tar"
 
-    articles, titles, vocabulary = preprocess.generate_vocabulary(relative_path, -1, True)
+    summary_pairs, vocabulary = load_dataset(relative_path)
 
-    encoder = EncoderRNN(vocabulary.n_words, hidden_size, n_layers=n_layers)
+    encoder = EncoderRNN(vocabulary.n_words, embedding_size, hidden_size, n_layers=n_layers)
 
-    max_length = max(len(article.split(">>>")[1].strip().split(' ')) for article in articles) + 1
+    max_article_length = max(len(pair.article_tokens) for pair in summary_pairs) + 1
+    max_abstract_length = max(len(pair.abstract_tokens) for pair in summary_pairs) + 1
 
-    decoder = AttnDecoderRNN(hidden_size, vocabulary.n_words, max_length=max_length, n_layers=n_layers,
-                             dropout_p=dropout_p)
+    decoder = PointerGeneratorDecoder(hidden_size, embedding_size, vocabulary.n_words, max_length=max_article_length,
+                                      n_layers=n_layers, dropout_p=dropout_p)
 
     if use_cuda:
         encoder = encoder.cuda()
@@ -106,17 +121,18 @@ if __name__ == '__main__':
     encoder.eval()
     decoder.eval()
 
-    # articles = articles[0:10000]
+    # summary_pairs = summary_pairs[0:1000]
 
     # generate data
-    fake_data = generate_argmax_summaries(vocabulary, encoder, decoder, articles, max_length, with_categories)
+    fake_data = generate_argmax_summaries(vocabulary, encoder, decoder, summary_pairs, max_article_length,
+                                          max_abstract_length)
 
     # for sample in fake_data:
     #     print(sample, flush=True)
 
     print("Writing to file", flush=True)
 
-    fake_data_save_file = "../../data/ntb_fake_data/test_generated_fake_data.unk.title.txt"
+    fake_data_save_file = "../../data/ntb_fake_data/pointer_test1.unk.title.txt"
     with open(fake_data_save_file, 'w') as file:
         for sample in fake_data:
             file.write(sample)
