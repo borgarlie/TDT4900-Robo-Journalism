951310104c5ecfa5f2503116d5f4979b79c7cfba
Borgar Lie <borgar.lie@gmail.com>
2018-02-28 12:04:58 +0100

    Make ROUGE calculation work with logger

--- evaluation/seq2seq/calculate_rouge.py
+++ evaluation/seq2seq/calculate_rouge.py
@@ -5,11 +5,14 @@ import json
 
 def read_file(path):
     text = open(path, encoding='utf-8').read()
+    text = clean_logger_output(text)  # when using logger
     titles = []
     output = []
     not_truth = False
     for line in text.split('\n'):
         if line.startswith('>'):
+            if not_truth:
+                titles.pop()  # temp fix when we apparently do not get all the output...
             not_truth = True
         elif line.startswith('='):
             titles.append(line[1:])
@@ -32,8 +35,19 @@ def clean_text(input_txt):
     return output_txt
 
 
+def clean_logger_output(text):
+    output = ""
+    for line in text.split('\n'):
+        cleaned_line = re.sub('^(.*?)INFO - ', '', line)
+        cleaned_line = re.sub('^(.*?)ERROR - ', '', cleaned_line)
+        if len(cleaned_line) > 0:
+            output += cleaned_line + '\n'
+    return output
+
+
 if __name__ == '__main__':
-    path = '../output_for_eval/pointer_gen_ntb_baseline_2.txt'
+    # path = '../output_for_eval/pointer_gen_ntb_baseline_2.txt'
+    path = '../output_for_eval/ntb_adagrad_test1.txt'
     print("Started extracting titles...")
     reference, hypothesis = read_file(path)
     print("Done extracting titles...")
